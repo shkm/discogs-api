03bcb82b6a979ea038205586305b5969e113f6f5
Jamie Schembri <jamie@schembri.me>
2018-08-19 18:37:45 +0200

    Artists#releases

--- lib/discogs/api/artists.rb
+++ lib/discogs/api/artists.rb
@@ -2,4 +2,8 @@ module Discogs::Api::Artists
   def self.get(client, artist_id)
     client.get Discogs::Api::Url.resolve('artists', artist_id)
   end
+
+  def self.releases(client, artist_id)
+    client.get Discogs::Api::Url.resolve('artists', artist_id, 'releases')
+  end
 end
--- spec/discogs/api/artists_spec.rb
+++ spec/discogs/api/artists_spec.rb
@@ -1,9 +1,8 @@
 RSpec.describe Discogs::Api::Artists do
   let(:client) { Discogs::Api::Client.new('token', 'user-agent') }
+  let(:artist_id) { 5272208 }
 
   describe '.get' do
-    let(:artist_id) { 5272208 }
-
     it 'sends an artist request' do
       with_fake_discogs_server do
         subject.get(client, artist_id)
@@ -19,6 +18,16 @@ RSpec.describe Discogs::Api::Artists do
         expect(response.status).to be_success
         expect(response.to_h).to have_keys %i[profile releases_url name uri urls
                                               images resource_url id data_quality]
+      end
+    end
+  end
+
+  describe '.releases' do
+    it 'sends a request for releases for the artist' do
+      with_fake_discogs_server do
+        subject.releases(client, artist_id)
+
+        expect_request(:get, "artists/#{artist_id}/releases")
       end
     end
   end
